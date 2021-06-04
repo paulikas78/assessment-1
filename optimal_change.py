cdf6463e9f0a2a4eac7a4a3ad28e9d727e05c3fd
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-04 13:04:02 -0500

    working process of optimal_change

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,29 @@
-# Write your solution here!
+def optimal_change(item_cost, amount_paid):
+    change = 100 * (amount_paid - item_cost)
+    change_dict = {}
+    
+    dollars = {
+          "one hundred": 10000,
+          "fifty": 5000,
+          "twenty": 2000,
+          "ten": 1000,
+          "five": 500,
+          "one": 100,
+          "quarter": 25,
+          "dime": 10,
+          "nickel": 5,
+          "penny": 1,
+          }
+
+    for currency in dollars:
+        while change >= dollars[currency]:
+
+          if currency in change_dict:
+            change_dict[currency] += 1
+          else:
+            change_dict[currency] = 1
+          change -= dollars[currency]
+    
+    print(change_dict)
+
+print(optimal_change(44.31, 100))
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,28 @@
-# Write your unit tests here!
+from roman_numerals import to_roman
+import unittest
+    
+class TestRomanNumerals(unittest.TestCase):
+    """Tests for 'roman_numerals.py'."""
+    #add 2 more tests
+    
+    """ Tests for a string returned."""
+    def test_returns_a_string(self):            
+        self.assertTrue(type(to_roman(5)) == str)
+    
+    """ Tests for output accuracy."""
+    def test_output_accuracy(self):        
+        self.assertTrue(to_roman(1) == "I")
+        self.assertTrue(to_roman(8) == "VIII")
+        self.assertTrue(to_roman(54) == "LIV")
+        self.assertTrue(to_roman(219) == "CCXIX")
+        
+    """ Challenge questions."""
+    def test_challenges(self):        
+        self.assertTrue(to_roman(2021) == "MMXXI")
+        self.assertTrue(to_roman(777) == "DCCLXXVII")
+        self.assertTrue(to_roman(939) == "CMXXXIX")
+        self.assertTrue(to_roman(94) == "XCIV")
+
+if __name__ == '__main__':
+    unittest.main()
+  
