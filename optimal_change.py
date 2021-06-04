7697b6b0d4582ab686cc297496f82745fefc6dce
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-04 16:09:23 -0500

    updated assessment 1 at 4:08 pm

--- optimal_change.py
+++ optimal_change.py
@@ -1,29 +1,63 @@
 def optimal_change(item_cost, amount_paid):
-    change = 100 * (amount_paid - item_cost)
-    change_dict = {}
     
-    dollars = {
-          "one hundred": 10000,
-          "fifty": 5000,
-          "twenty": 2000,
-          "ten": 1000,
-          "five": 500,
-          "one": 100,
+    return_str = ""      # creates string that will return information about change (or lack thereof).
+    
+    
+    # converts all dollar amounts into cents, so units are uniform (e.g. $10 == 1000 cents), and finds change; difference of item_cost and amount_paid.
+    
+    change = 100 * (amount_paid - item_cost) 
+    
+    change_dict = {}                # blank dictionary created to store change currency and quantity.
+    
+    dollars = {      
+          "$100 bill": 10000,       # currency (keys) listed alongside dollar amounts in cents (values).
+          "$50 bill": 5000,
+          "$20 bill": 2000,
+          "$10 bill": 1000,
+          "$5 bill": 500,
+          "$1 bill": 100,
           "quarter": 25,
           "dime": 10,
           "nickel": 5,
-          "penny": 1,
+          "penn": 1,
           }
 
-    for currency in dollars:
-        while change >= dollars[currency]:
 
-          if currency in change_dict:
-            change_dict[currency] += 1
-          else:
-            change_dict[currency] = 1
-          change -= dollars[currency]
-    
-    print(change_dict)
+# assesses whether exact change or insufficient funds are tendered.
 
-print(optimal_change(44.31, 100))
+    if item_cost > amount_paid:      
+        return_str += "Insufficient funds.  Please add money to make a purchase."
+    elif item_cost == amount_paid and item_cost > 0:
+        return_str += "Thank you for using exact change.  Have a nice day!"
+    elif item_cost == amount_paid and item_cost == 0:
+        return_str += "Please make a selection."
+    else:
+
+
+# interate through "dollars" dictionary, looking to match currency denomination with corresponding value converted into cents.
+
+        for currency in dollars: 
+            while change >= dollars[currency]:    # assessing change on hand with dollars dictionary.
+                                                
+                if currency in change_dict:       # assessing if currency exists in change_dict.
+                    change_dict[currency] += 1   
+                else:
+                    change_dict[currency] = 1     # addidng to change_dict, accordingly.
+                change -= dollars[currency]       # finally, subtract dictionary value used from change.
+                
+                
+        return_str += f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is " # adds to return_str with change information.
+          
+        for change_amount in change_dict: # iterates through change_dict & adds to string with its value.
+            
+            if change_dict[change_amount] == 1 and change_amount != "penn":
+                return_str += f"{change_dict[change_amount]} {change_amount}, " 
+            elif change_dict[change_amount] > 1 and change_amount != "penn":
+                return_str += f"{change_dict[change_amount]} {change_amount}s, "
+            elif change_dict[change_amount] == 1 and change_amount == "penn":
+                return_str += f"and {change_dict[change_amount]} {change_amount}y."
+            elif change_dict[change_amount] > 1 and change_amount == "penn":
+                return_str += f"and {change_dict[change_amount]} {change_amount}ies."
+          
+    return return_str # returns constructed string indicating change & subsequent currency denominations.
+
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,27 +1,33 @@
-from roman_numerals import to_roman
+from optimal_change import optimal_change
 import unittest
     
 class TestRomanNumerals(unittest.TestCase):
-    """Tests for 'roman_numerals.py'."""
-    #add 2 more tests
+    """Tests for 'optimal_change.py'."""
+   
     
     """ Tests for a string returned."""
     def test_returns_a_string(self):            
-        self.assertTrue(type(to_roman(5)) == str)
+        self.assertTrue(type(optimal_change(5, 20)) == str)
+        
+    """ Tests for insufficient funds."""
+    def test_returns_insufficient_funds(self):
+        self.assertTrue(optimal_change(101, 100) == "You don't have enough money.")
+        
+    """ Tests for exact change."""
+    def test_returns_if_exact_change_used(self):
+        self.assertTrue(optimal_change(100, 100) == "Thank you for using exact change.  Have a nice day!")
     
     """ Tests for output accuracy."""
     def test_output_accuracy(self):        
-        self.assertTrue(to_roman(1) == "I")
-        self.assertTrue(to_roman(8) == "VIII")
-        self.assertTrue(to_roman(54) == "LIV")
-        self.assertTrue(to_roman(219) == "CCXIX")
+        self.assertTrue(optimal_change(5.99, 10) == "The optimal change for an item that costs $5.99 with an amount paid of $10 is 4 $1 bills, and 1 penny.")
+        self.assertTrue(optimal_change(2.43, 20) == "The optimal change for an item that costs $2.43 with an amount paid of $20 is 1 $10 bill, 1 $5 bill, 2 $1 bills, 2 quarters, 1 nickel, and 2 pennies.")
+        self.assertTrue(optimal_change(22.49, 50) == "The optimal change for an item that costs $22.49 with an amount paid of $50 is 1 $20 bill, 1 $5 bill, 2 $1 bills, 2 quarters, and 1 penny.")
         
     """ Challenge questions."""
     def test_challenges(self):        
-        self.assertTrue(to_roman(2021) == "MMXXI")
-        self.assertTrue(to_roman(777) == "DCCLXXVII")
-        self.assertTrue(to_roman(939) == "CMXXXIX")
-        self.assertTrue(to_roman(94) == "XCIV")
+        self.assertTrue(optimal_change(110.41, 200) == "The optimal change for an item that costs $110.41 with an amount paid of $200 is 1 $50 bill, 1 $20 bill, 1 $10 bill, 1 $5 bill, 4 $1 bills, 2 quarters, 1 nickel, and 4 pennies.")
+        self.assertTrue(optimal_change(303.01, 400) == "The optimal change for an item that costs $303.01 with an amount paid of $400 is 1 $50 bill, 2 $20 bills, 1 $5 bill, 1 $1 bill, 3 quarters, 2 dimes, and 4 pennies.")
+        
 
 if __name__ == '__main__':
     unittest.main()
